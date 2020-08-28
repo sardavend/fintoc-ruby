06d24cf10f58679c9ad1380252acc438b1d4c9ce
sardavend <juan.carlos.sardin@gmail.com>
2020-08-28 00:00:49 -0400

    test(insitution):add instituion examples group

---
+++ spec/lib/fintoc/resources/institution_spec.rb
@@ -0,0 +1,13 @@
+require 'fintoc/resources/institution'
+
+RSpec.describe Fintoc::Institution do
+  let(:data) { { id_: 'cl_banco_de_chile', name: 'Banco de Chile', country: 'cl'} }
+  let(:institution) { Fintoc::Institution.new(**data)}
+  it 'create an instance of Institution' do
+    expect(institution).to be_an_instance_of(Fintoc::Institution)
+  end
+  it "returns the institution's name" do
+    expect(institution.to_s).to eq("🏦 #{data[:name]}")
+
+  end
+end
