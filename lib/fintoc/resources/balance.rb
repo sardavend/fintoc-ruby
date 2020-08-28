36a49417f086625aad98d5108f1d91f7867a9b52
sardavend <juan.carlos.sardin@gmail.com>
2020-08-27 23:59:40 -0400

    feat(balance):add Balance Class implemententation

---
+++ lib/fintoc/resources/balance.rb
@@ -0,0 +1,21 @@
+module Fintoc
+  class Balance
+    def initialize(available:, current:, limit:)
+      @available = available
+      @current = current
+      @limit = limit
+    end
+
+    def id_
+      object_id
+    end
+
+    def to_s
+      "#{@available} (#{@current}"
+    end
+
+    def inspect
+      "<Balance #{@available} (#{@current}>"
+    end
+  end
+end
