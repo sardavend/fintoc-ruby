d9135e80d5c3f8f03fcecd45d79b7da327d790d3
sardavend <juan.carlos.sardin@gmail.com>
2020-08-29 01:18:46 -0400

    feat(movement):implement movement resource class

---
+++ lib/fintoc/resources/movement.rb
@@ -0,0 +1,39 @@
+require 'date'
+require 'fintoc/resources/transfer_account'
+
+module Fintoc
+  class Movement
+    def initialize(
+      id:,
+      amount:,
+      currency:,
+      description:,
+      post_date:,
+      transaction_date:,
+      type:,
+      recipient_account:,
+      sender_account:,
+      comment:,
+      **
+    )
+      @id = id
+      @amount = amount
+      @currency = currency
+      @description = description
+      @post_date = Date.iso8601(post_date)
+      @transaction_date = transaction_date and Date.iso8601(transaction_date)
+      @type = type
+      @recipient_account = recipient_account and Fintoc::TransferAccount.new(**recipient_account)
+      @sender_account = sender_account and Fintoc::TransferAccount.new(**sender_account)
+      @comment = comment
+    end
+
+    def locale_date
+      @post_date.strftime('%x')
+    end
+
+    def to_s
+      "#{@amount} (#{@description} @ #{locale_date})"
+    end
+  end
+end
