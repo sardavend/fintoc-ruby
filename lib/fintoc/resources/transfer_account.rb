517bfe811de7cdb7069978e641c29ded6d123972
sardavend <juan.carlos.sardin@gmail.com>
2020-08-29 01:19:27 -0400

    feat(transfer_account):implement resource transfer_account class

---
+++ lib/fintoc/resources/transfer_account.rb
@@ -0,0 +1,20 @@
+require 'fintoc/resources/institution'
+
+module Fintoc
+  class TransferAccount
+    def initialize(holder_id:, holder_name:, number:, institution:, **)
+      @holder_id = holder_id
+      @holder_name = holder_name
+      @number = number
+      @institution = institution and Fintoc::Institution.new(**institution)
+    end
+
+    def id_
+      object_id
+    end
+
+    def to_s
+      @holder_id.to_s
+    end
+  end
+end
