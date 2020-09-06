61bf586771b1c07fe695b7bdd0365aa906f0a37c
sardavend <juan.carlos.sardin@gmail.com>
2020-09-06 03:07:44 -0300

    test(balance): remove unncessary splating the arguments

--- spec/lib/fintoc/resources/balance_spec.rb
+++ spec/lib/fintoc/resources/balance_spec.rb
@@ -2,7 +2,7 @@ require 'fintoc/resources/balance'
 
 RSpec.describe Fintoc::Balance do
   let(:data) { { available: 1000, current: 500, limit: 10 } }
-  let(:balance) { Fintoc::Balance.new(**data)}
+  let(:balance) { Fintoc::Balance.new(data) }
   it 'create an instance of Balance' do
     expect(balance).to be_an_instance_of(Fintoc::Balance)
   end
