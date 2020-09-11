58e6a81cca0a9c574c3e4702b9bc19b85127cfb8
sardavend <juan.carlos.sardin@gmail.com>
2020-09-11 15:53:58 -0300

    test(account_spec):add movements and update_balance examples

--- spec/lib/fintoc/resources/account_spec.rb
+++ spec/lib/fintoc/resources/account_spec.rb
@@ -1,6 +1,9 @@
 require 'fintoc/resources/account'
 
 RSpec.describe Fintoc::Account do
+  let(:api_key) { 'sk_test_9c8d8CeyBTx1VcJzuDgpm4H-bywJCeSx' }
+  let(:link_token) { '6n12zLmai3lLE9Dq_token_gvEJi8FrBge4fb3cz7Wp856W' }
+  let(:client) { Fintoc::Client.new(api_key) }
   let(:data) do
     {
       "id": 'Z6AwnGn4idL7DPj4', "name": 'Cuenta Corriente',
@@ -12,18 +15,44 @@ RSpec.describe Fintoc::Account do
         "available": 7_010_510,
         "current": 7_010_510,
         "limit": 7_510_510
-      }
+      },
+      "client": client
     }
   end
+  let(:link) { client.link(link_token) }
   let(:account) { Fintoc::Account.new(**data) }
-  it 'create an instance of Account' do
-    expect(account).to be_an_instance_of(Fintoc::Account)
+  describe '#new' do
+    it 'create an instance of Account' do
+      expect(account).to be_an_instance_of(Fintoc::Account)
+    end
   end
-
-  it "print the account's holder_name, name, and balance when to_s is called" do
-    expect(account.to_s)
-      .to eq(
-        "💰 #{data[:holder_name]}’s #{data[:name]} #{data[:balance][:available]} (#{data[:balance][:current]})"
-      )
+  describe '#to_s' do
+    it "print the account's holder_name, name, and balance when to_s is called" do
+      expect(account.to_s)
+        .to eq(
+          "💰 #{data[:holder_name]}’s #{data[:name]} #{data[:balance][:available]} (#{data[:balance][:current]})"
+        )
+    end
+  end
+  let(:linked_account) { link.find(type: 'checking_account') }
+  describe '#movements' do
+    it "get the last 30 account's movements", :vcr do
+      movements = linked_account.movements
+      expect(movements.size).to be <= 30
+      expect(movements).to all(be_a(Fintoc::Movement))
+    end
+  end
+  describe '#movement with since argument' do
+    it "get account's movements with arguments", :vcr do
+      movements = linked_account.movements(since: '2020-08-15')
+      linked_account.show_movements
+      expect(movements).to all(be_a(Fintoc::Movement))
+    end
+  end
+  describe '#update_balance' do
+    it "update account's movements", :vcr do
+      movements = linked_account.update_movements
+      expect(movements).to all(be_a(Fintoc::Movement))
+    end
   end
 end
