eca7058f1067f0881eaa766cfbc0563a96efe240
sardavend <juan.carlos.sardin@gmail.com>
2020-09-11 15:28:05 -0300

    test(client_spec): remove not related examples and add vcr tapes

--- spec/lib/fintoc/client_spec.rb
+++ spec/lib/fintoc/client_spec.rb
@@ -5,38 +5,34 @@ require 'fintoc/resources/movement'
 
 RSpec.describe Fintoc::Client do
   let(:api_key) { 'sk_test_9c8d8CeyBTx1VcJzuDgpm4H-bywJCeSx' }
-  let(:link_token) {'6n12zLmai3lLE9Dq_token_gvEJi8FrBge4fb3cz7Wp856W'}
+  let(:link_token) { '6n12zLmai3lLE9Dq_token_gvEJi8FrBge4fb3cz7Wp856W' }
   let(:client) { Fintoc::Client.new(api_key) }
-
-  it 'create an instance Client' do
-    expect(client).to be_an_instance_of(Fintoc::Client)
+  describe '#new' do
+    it 'create an instance Client' do
+      expect(client).to be_an_instance_of(Fintoc::Client)
+    end
   end
 
-  context 'Client - API Interactions' do
-    let(:link) { client.link(link_token) }
-    let(:account) { link.find(type: 'checking_account') }
-    it 'get the link from a given link token' do
+  describe '#link' do
+    it 'get the link from a given link token', :vcr do
+      link = client.link(link_token)
       expect(link).to be_an_instance_of(Fintoc::Link)
     end
-    it 'get a valid account' do
-      expect(account).to be_an_instance_of(Fintoc::Account)
-    end
-    it "get account's movements without arguments" do
-      movements = account.movements
-      expect(movements.size).to be <= 30
-      expect(movements).to all(be_a(Fintoc::Movement))
-    end
-    it "get account's movements with arguments" do
-      movements = account.movements(since: '2020-08-15')
-      account.show_movements
-      expect(movements).to all(be_a(Fintoc::Movement))
-    end
+  end
 
-    it "update account's movements" do
-      movements = account.movements(since: '2020-08-15')
-      account.update_balance
-      expect(movements).to all(be_a(Fintoc::Movement))
+  describe '#links' do
+    it 'get all the links from a given link token', :vcr do
+      links = client.links
+      expect(links).to all(be_a(Fintoc::Link))
     end
+  end
 
+  describe '#account' do
+    it 'get a link account', :vcr do
+      link = client.link(link_token)
+      account = link.find(type: 'checking_account')
+      returned_account = client.account(link_token, account.id)
+      expect(returned_account).to be_an_instance_of(Fintoc::Account)
+    end
   end
 end
