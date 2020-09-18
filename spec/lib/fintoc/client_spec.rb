7245ce613115ef712c74f2d30b36516e4129bf2a
sardavend <juan.carlos.sardin@gmail.com>
2020-09-18 17:04:15 -0300

    test(client_spec): call to get_link instead of link method invocation

--- spec/lib/fintoc/client_spec.rb
+++ spec/lib/fintoc/client_spec.rb
@@ -12,26 +12,23 @@ RSpec.describe Fintoc::Client do
       expect(client).to be_an_instance_of(Fintoc::Client)
     end
   end
-
-  describe '#link' do
+  describe '#get_link' do
     it 'get the link from a given link token', :vcr do
-      link = client.link(link_token)
+      link = client.get_link(link_token)
       expect(link).to be_an_instance_of(Fintoc::Link)
     end
   end
-
-  describe '#links' do
+  describe '#get_links' do
     it 'get all the links from a given link token', :vcr do
-      links = client.links
+      links = client.get_links
       expect(links).to all(be_a(Fintoc::Link))
     end
   end
-
-  describe '#account' do
-    it 'get a link account', :vcr do
-      link = client.link(link_token)
+  describe '#get_account' do
+    it 'get a linked account', :vcr do
+      link = client.get_link(link_token)
       account = link.find(type: 'checking_account')
-      returned_account = client.account(link_token, account.id)
+      returned_account = client.get_account(link_token, account.id)
       expect(returned_account).to be_an_instance_of(Fintoc::Account)
     end
   end
